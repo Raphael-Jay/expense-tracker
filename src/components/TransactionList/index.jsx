bd9c1b53973839d6bcf96001fe94fdde2c40a59f
Raphael Jay <iteachmyselfwebdev@gmail.com>
2022-09-09 21:42:10 +0800

    transaction list

--- src/components/TransactionList/index.jsx
+++ src/components/TransactionList/index.jsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react'
+import { GlobalContext } from './../../context/GlobalState'
+
 import Transaction from './../Transaction'
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext)
+  console.log(transactions)
   return (
     <>
       <h3>History</h3>
       <ul className='list'>
-        <li className='minus'>
-          Cash <span>-400</span>
-          <button className='delete-btn'>x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <li key={transaction.id} className='minus'>
+            {transaction.text} <span>{transaction.amount}</span>
+            <button className='delete-btn'>x</button>
+          </li>
+        ))}
       </ul>
     </>
   )
